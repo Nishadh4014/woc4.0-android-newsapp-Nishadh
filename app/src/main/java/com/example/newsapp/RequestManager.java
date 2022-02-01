3401d7b509b7dd28beffa80aa4c071a00a64ae81
Nishadh4014 <74643042+Nishadh4014@users.noreply.github.com>
2022-02-01 22:29:02 +0530

    Update RequestManager.java

--- app/src/main/java/com/example/newsapp/RequestManager.java
+++ app/src/main/java/com/example/newsapp/RequestManager.java
@@ -21,7 +21,7 @@ public class RequestManager {
     public void getNewsHeadlines(OnFetchDataListener listener, String category, String query)
     {
         CallNewsApi callNewsApi = retrofit.create(CallNewsApi.class);
-        Call<NewsApiResponse> call = callNewsApi.callHeadlines("in",category,query,context.getString(R.string.api_key));
+        Call<NewsApiResponse> call = callNewsApi.callHeadlines("in",category,query,context.getString(R.string.api_key),100);
 
         try{
             call.enqueue(new Callback<NewsApiResponse>() {
@@ -55,7 +55,8 @@ public class RequestManager {
                 @Query("country") String country,
                 @Query("category") String category,
                 @Query("q") String query,
-                @Query("apiKey") String api_key
+                @Query("apiKey") String api_key,
+                @Query("pageSize") int page_size
         );
     }
 }
